6d24c7dcca5e8d80275dba2e3344af01f65dc7ec
Esra Almughrabi <e.almughrabi02@gmail.com>
2025-05-22 00:48:38 +0200

    first working test in cypress 🥳

--- cypress/e2e/firstTest.cy.ts
+++ cypress/e2e/firstTest.cy.ts
@@ -1,5 +1,21 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("LoginView E2E Test", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:5173/");
+  });
+
+  it("logs in successfully and redirects to dashboard", () => {
+    cy.get('[data-testid="email-input"]').type("test@test.com");
+    cy.get('[data-testid="password-input"]').type("secure101");
+    cy.get('[data-testid="sign-in-btn"]').click();
+
+    cy.url().should("include", "/dashboard");
+  });
+
+  it("shows an error message on failed login", () => {
+    cy.get('[data-testid="email-input"]').type("wrong@example.com");
+    cy.get('[data-testid="password-input"]').type("wrongpass");
+    cy.get('[data-testid="sign-in-btn"]').click();
+
+    cy.contains("Invalid email or password").should("be.visible");
   });
 });
