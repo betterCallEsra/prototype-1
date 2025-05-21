37375c2d464ba4aaf21ea409acb5051184ad95a2
Esra Almughrabi <e.almughrabi02@gmail.com>
2025-05-21 09:36:59 +0200

    commented out entries and relying on app

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,10 +1,10 @@
-import { StaticRouter } from "react-router-dom";
-import App from "./App";
+// import { StaticRouter } from "react-router-dom";
+// import App from "./App";
 
-export function render(url: string) {
-  return (
-    <StaticRouter location={url}>
-      <App />
-    </StaticRouter>
-  );
-}
+// export function render(url: string) {
+//   return (
+//     <StaticRouter location={url}>
+//       <App />
+//     </StaticRouter>
+//   );
+// }
